54325b247574fbd7448bd4567c07d46bf4f7a111
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-05-25 21:59:55 +0200

    add: #3 - golang server

---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module github.com/blyndusk/image-resizer/api
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.2
---
+++ api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	setupServer()
+}
+
+func setupServer() *gin.Engine {
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "[image-resizer]",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3001")
+	return r
+}
