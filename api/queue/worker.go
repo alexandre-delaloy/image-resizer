a024cee76f19f8874947d6c9bf84c6cefa89a933
Damien-MATHIEU <damien.mathieu612@gmail.com>
2021-06-19 16:39:17 +0200

    Message aknowlege

--- api/queue/worker.go
+++ api/queue/worker.go
@@ -36,7 +36,7 @@ func main() {
 	msgs, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
-		true,   // auto-ack
+		false,   // auto-ack
 		false,  // exclusive
 		false,  // no-local
 		false,  // no-wait
@@ -48,11 +48,12 @@ func main() {
 	  
 	  go func() {
 		for d := range msgs {
-		  log.Printf("Received a message: %s", d.Body)
-		  dotCount := bytes.Count(d.Body, []byte("."))
-		  t := time.Duration(dotCount)
-		  time.Sleep(t * time.Second)
-		  log.Printf("Done")
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false)
 		}
 	  }()
 	  
