61f83a80893e9fb5ed1ef07d8fd7dda3afac31df
Damien-MATHIEU <damien.mathieu612@gmail.com>
2021-06-20 15:04:48 +0200

    Handle create user and update user

--- api/queue/emit_create_user.go
+++ api/queue/emit_create_user.go
@@ -24,22 +24,22 @@ func main() {
         defer ch.Close()
 
         err = ch.ExchangeDeclare(
-                "logs_direct", // name
-                "direct",      // type
-                true,          // durable
-                false,         // auto-deleted
-                false,         // internal
-                false,         // no-wait
-                nil,           // arguments
+                "create_user",   // name
+                "direct", // type
+                true,     // durable
+                false,    // auto-deleted
+                false,    // internal
+                false,    // no-wait
+                nil,      // arguments
         )
         failOnError(err, "Failed to declare an exchange")
 
         body := bodyFrom(os.Args)
         err = ch.Publish(
-                "logs_direct",         // exchange
-                severityFrom(os.Args), // routing key
-                false, // mandatory
-                false, // immediate
+                "create_user", // exchange
+                "",     // routing key
+                false,  // mandatory
+                false,  // immediate
                 amqp.Publishing{
                         ContentType: "text/plain",
                         Body:        []byte(body),
@@ -51,20 +51,10 @@ func main() {
 
 func bodyFrom(args []string) string {
         var s string
-        if (len(args) < 3) || os.Args[2] == "" {
-                s = "hello"
+        if (len(args) < 2) || os.Args[1] == "" {
+                s = "Create user"
         } else {
-                s = strings.Join(args[2:], " ")
+                s = strings.Join(args[1:], " ")
         }
         return s
 }
-
-func severityFrom(args []string) string {
-        var s string
-        if (len(args) < 2) || os.Args[1] == "" {
-                s = "info"
-        } else {
-                s = os.Args[1]
-        }
-        return s
-}	
--- api/queue/receive_create_user.go
+++ api/queue/receive_create_user.go
@@ -2,7 +2,6 @@ package main
 
 import (
         "log"
-        "os"
 
         "github.com/streadway/amqp"
 )
@@ -23,13 +22,13 @@ func main() {
         defer ch.Close()
 
         err = ch.ExchangeDeclare(
-                "logs_direct", // name
-                "direct",      // type
-                true,          // durable
-                false,         // auto-deleted
-                false,         // internal
-                false,         // no-wait
-                nil,           // arguments
+                "create_user",   // name
+                "direct", // type
+                true,     // durable
+                false,    // auto-deleted
+                false,    // internal
+                false,    // no-wait
+                nil,      // arguments
         )
         failOnError(err, "Failed to declare an exchange")
 
@@ -43,29 +42,22 @@ func main() {
         )
         failOnError(err, "Failed to declare a queue")
 
-        if len(os.Args) < 2 {
-                log.Printf("Usage: %s [info] [warning] [error]", os.Args[0])
-                os.Exit(0)
-        }
-        for _, s := range os.Args[1:] {
-                log.Printf("Binding queue %s to exchange %s with routing key %s",
-                        q.Name, "logs_direct", s)
-                err = ch.QueueBind(
-                        q.Name,        // queue name
-                        s,             // routing key
-                        "logs_direct", // exchange
-                        false,
-                        nil)
-                failOnError(err, "Failed to bind a queue")
-        }
+        err = ch.QueueBind(
+                q.Name, // queue name
+                "",     // routing key
+                "create_user", // exchange
+                false,
+                nil,
+        )
+        failOnError(err, "Failed to bind a queue")
 
         msgs, err := ch.Consume(
                 q.Name, // queue
                 "",     // consumer
-                true,   // auto ack
+                false,   // auto-ack
                 false,  // exclusive
-                false,  // no local
-                false,  // no wait
+                false,  // no-local
+                false,  // no-wait
                 nil,    // args
         )
         failOnError(err, "Failed to register a consumer")
@@ -78,6 +70,6 @@ func main() {
                 }
         }()
 
-        log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+        log.Printf(" [*]. To exit press CTRL+C")
         <-forever
 }
