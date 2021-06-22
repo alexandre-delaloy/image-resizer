1145ad8ce4e54f2dc9f8164811da43280e40e622
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-06-22 15:36:04 +0200

    add: #5 - link file upload to queue

--- api/controllers/upload.go
+++ api/controllers/upload.go
@@ -1,17 +1,25 @@
 package controllers
 
 import (
+	"math/rand"
 	"net/http"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 func UploadFile(c *gin.Context) {
 
 	file, err := c.FormFile("file")
-
+	extension := filepath.Ext(file.Filename)
+	// if extension != "png" {
+	// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+	// 		"message": "Only PNG files are supported",
+	// 	})
+	// 	return
+	// }
 	// The file cannot be received.
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
@@ -19,14 +27,14 @@ func UploadFile(c *gin.Context) {
 		})
 		return
 	}
-
+	s1 := rand.NewSource(time.Now().Unix())
+	r1 := rand.New(s1)
 	// Retrieve file information
-	extension := filepath.Ext(file.Filename)
 	// Generate random file name for the new uploaded file so it doesn't override the old file with same name
-	newFileName := uuid.New().String() + extension
+	newFileName := strconv.Itoa(r1.Intn(100000)) + extension
 
 	// The file is received, so let's save it
-	if err := c.SaveUploadedFile(file, "./"+newFileName); err != nil {
+	if err := c.SaveUploadedFile(file, "./service-worker/producer/test-images/"+newFileName); err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 			"message": "Unable to save the file",
 		})
--- api/go.mod
+++ api/go.mod
@@ -4,9 +4,9 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
-	github.com/google/uuid v1.2.0
 	github.com/joho/godotenv v1.3.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/streadway/amqp v1.0.0
 	gorm.io/driver/postgres v1.1.0
